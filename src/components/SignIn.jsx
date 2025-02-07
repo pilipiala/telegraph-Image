b96d9929f532126533ef2e5af2b8239664d1434a
啊茶kk <acgr18share@gmail.com>
2025-02-07 20:11:21 +0800

    更新ico

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -39,7 +39,7 @@ export function LoginPage() {
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
+          啊茶茶大人专属登录页
         </h1>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" onSubmit={handleSubmit}>
