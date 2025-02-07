8504e0bcc587dcd067193f7e745d84188f1043ad
啊茶kk <acgr18share@gmail.com>
2025-02-07 19:18:53 +0800

    更改图床

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ export default function Footer() {
   return (
     <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
       <div >
-        <p className="text-xs text-gray-500">Copyright Ⓒ 2025 All rights reserved. 不要乱传图片上来，抓到打一顿。 '(゜-゜)つロ 干杯~
+        <p className="text-xs text-gray-500">Copyright Ⓒ 2025 All rights reserved. 不要乱传图片上来，抓到打一顿。
           <Link 
-          href="https://pilipiala.us.kg/"
+          href="https://pilipiala.us.kg"
           className="text-blue-300  hover:text-red-900 ml-1"
           target="_blank"
           rel="noopener noreferrer"
-          >啊茶茶の小窝</Link> </p>
+          >啊茶茶'(゜-゜)つロ 干杯~</Link> </p>
       </div>
     </footer>
   );
